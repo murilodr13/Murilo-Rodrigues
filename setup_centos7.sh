fda075a3b64b5c06537d3e9c1dc7b6633ccb44ff
Murilo Rodrigues <mrodrigues@ibm.com>
2021-01-29 18:02:33 -0300

    Instalador CentOS 7.x VM v.1.0.2

--- setup_centos7.sh
+++ setup_centos7.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
 
-#Script para Configuração Inicial de Servidor CentosOS 7.x
+#############################################################
+# Script para Configuração Inicial de Servidor CentosOS 7.x #
+#-----------------------------------------------------------#
+# v.1.0 - Criação do script.                                #
+# v.1.0.1 - Adicionado o comando yum.                       #
+#############################################################
+
+###########
+BLUEL="\033[1;34m"
+BLUE="\033[0;34m"
+GREEN="\033[0;32m"
+
+
+echo ""
+echo ""
+
+echo -e " ${BLUEL} ====== Script para Configuração Inicial de Servidor CentosOS 7.x ======  "
 
 echo ""
 
-echo -e " \033[1;34m ====== Script para Configuração Inicial de Servidor CentosOS 7.x ====== \033[0m "
-
-echo ""
-
-echo -e " \033[0;34m Verificando Hostname \033[0m "
+echo -e " ${BLUE} ====== Verificando Hostname ====== "
 hostname
 
 sleep 3
 
 echo ""
 
-echo -e " \033[0;34m Verificando Usuário \033[0m "
+echo -e " ${BLUE} ====== Verificando Usuário ====== "
 whoami
 
 sleep 3
 
 echo ""
 
-echo -e " \033[0;34m Iniciando atualizações do Sistema (Sem Atualizações de Kernel) \033[0m "
+echo -e " ${BLUE} ====== Iniciando atualizações do Sistema (Sem Atualizações de Kernel) ====== "
 
-yum --exclude=kernel* update -y
+sudo yum --exclude=kernel* update
 
 sleep 5
 
-echo -e " \033[0;32m Fim do Script \033[0m "
+echo -e " ${GREEN} ====== Fim do Script ====== "
