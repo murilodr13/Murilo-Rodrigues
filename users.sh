95f75d7baffe5b36307b27ecef8710ecb5db536a
Murilo Rodrigues <mrodrigues@ibm.com>
2021-07-27 09:56:45 -0300

    Novos Arquivos

---
+++ users.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+echo ""
+echo "======= Hostname ======="
+echo $HOSTNAME
+echo "========================"
+echo ""
+echo ""
+echo "======================= Last Login by Username ======================="
+
+for username in $(getent passwd | cut -d ":"  -f1) ; do
+    last_login=$(last $username| head -1 | awk '{ print $4, $5, $6 }')
+    if [  ${#last_login} -le 5 ]; then
+        last_login="No info"
+    fi
+    hostname=$(hostname -s)
+    ugecos=$(getent passwd $username | cut -d ":"  -f5)
+    ushell=$(getent passwd $username | cut -d ":"  -f6)
+    echo $username","$last_login","$ugecos","$ushell","$hostname
+done
+
+echo ""
